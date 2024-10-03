bdda1f282fa9718dd67c5e3feb20a16e24da6f30
Roney <guddorx@gmail.com>
2024-10-03 10:03:53 +0600

    First commit V 1.0

---
+++ src/test/java/cns/tms/TollManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cns.tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
